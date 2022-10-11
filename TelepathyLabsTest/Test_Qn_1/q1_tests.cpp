424ee81c52c4133ecf8cc89ee18e63a7a4035e4a
Michael Ang <michael.ang@mytona.com>
2022-10-11 10:49:26 +0800

    q1 test comments

--- TelepathyLabsTest/Test_Qn_1/q1_tests.cpp
+++ TelepathyLabsTest/Test_Qn_1/q1_tests.cpp
@@ -39,8 +39,9 @@ TEST_F(RoomManagerTest, RequestAndAssignRoom) {
 	EXPECT_EQ(rm_test.requestAndAssignRoom(), "1B");
 	EXPECT_EQ(rm_test.requestAndAssignRoom(), "1C");
 	EXPECT_EQ(rm_test.requestAndAssignRoom(), "1D");
+	// test next room assigned always nearest
 	EXPECT_EQ(rm_test.requestAndAssignRoom(), "2E");
-
+	// extra test for nearest assignment
 	rm_test.roomCheckout("1C");
 	rm_test.roomCleaned("1C");
 	EXPECT_EQ(rm_test.requestAndAssignRoom(), "1C");
@@ -51,13 +52,13 @@ TEST_F(RoomManagerTest, CheckoutRoom) {
 	rm_test.requestAndAssignRoom();
 	rm_test.requestAndAssignRoom();
 	rm_test.requestAndAssignRoom();
-
+	// test correct & incorrect checkout as well as string input invariance
 	EXPECT_TRUE(rm_test.roomCheckout("1B"));
 	EXPECT_FALSE(rm_test.roomCheckout("1b"));
 
 	EXPECT_TRUE(rm_test.roomCheckout("1d"));
 	EXPECT_FALSE(rm_test.roomCheckout("1D"));
-
+	// test invalid checkout
 	EXPECT_FALSE(rm_test.roomCheckout("3C"));
 }
 
@@ -65,9 +66,10 @@ TEST_F(RoomManagerTest, CleanRoom) {
 	rm_test.requestAndAssignRoom();
 	rm_test.requestAndAssignRoom();
 	rm_test.roomCheckout("1B");
-
+	// test correct & incorrect room clean
 	EXPECT_TRUE(rm_test.roomCleaned("1b"));
 	EXPECT_FALSE(rm_test.roomCleaned("1B"));
+	// test invalid room clean
 	EXPECT_FALSE(rm_test.roomCleaned("1C"));
 	EXPECT_FALSE(rm_test.roomCleaned("4E"));
 }
@@ -76,10 +78,10 @@ TEST_F(RoomManagerTest, RepairingRoom) {
 	rm_test.requestAndAssignRoom();
 	rm_test.requestAndAssignRoom();
 	rm_test.roomCheckout("1B");
-
+	// test correct & incorrect room repair
 	EXPECT_TRUE(rm_test.roomInRepair("1b"));
 	EXPECT_FALSE(rm_test.roomInRepair("1B"));
-
+	// test invalid room repair
 	EXPECT_FALSE(rm_test.roomInRepair("1A"));
 	EXPECT_FALSE(rm_test.roomInRepair("1C"));
 	EXPECT_FALSE(rm_test.roomInRepair("2C"));
@@ -92,10 +94,10 @@ TEST_F(RoomManagerTest, RoomRepaired) {
 	rm_test.roomCheckout("1B");
 	rm_test.roomInRepair("1B");
 	rm_test.roomCheckout("1C");
-
+	// test correct & incorrect room repaired
 	EXPECT_TRUE(rm_test.roomRepaired("1b"));
 	EXPECT_FALSE(rm_test.roomRepaired("1B"));
-
+	// test invalid room repaired
 	EXPECT_FALSE(rm_test.roomRepaired("1A"));
 	EXPECT_FALSE(rm_test.roomRepaired("1C"));
 	EXPECT_FALSE(rm_test.roomRepaired("1D"));
@@ -114,6 +116,6 @@ TEST_F(RoomManagerTest, ListAllAvailable) {
 		"2D", "2C", "2B", "2A",
 		"3A", "3B", "3C", "3D", "3E",
 		"4E", "4D", "4C", "4B", "4A" };
-
+	// test correct listing operation
 	EXPECT_EQ(rm_test.listAllAvailableRooms(), ava_rms);
 }
